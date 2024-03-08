6fadeb6c2f7d3603d3ab7d54c65f4b29652219d2
nakyeonko3 <badaclock@gmail.com>
2024-03-09 00:02:38 +0900

    Remove : ãíílibraryapp 관련 코드만 남김. 나머지는 study ã브랜치에 넣음

--- src/main/java/com/group/Addressapp/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.group.Addressapp;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address,Long> {
-
-}
--- src/main/java/com/group/Diceapp/DiceAppApplication.java
+++
@@ -1,8 +0,0 @@
-package com.group.Diceapp;
-
-import com.group.Diceapp.view.DiceView;
-public class DiceAppApplication {
-    public static void main(String[] args) {
-        new DiceView().diceRollResultView();
-    }
-}
